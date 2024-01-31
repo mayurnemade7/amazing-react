546022f559d5a3d170fc0835553b47262e856218
unknown <mayur.nemade@gunnebo.com>
2024-01-31 17:28:46 +0530

    react from scratch

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * <div id="parent">
+ *      <div="child1">
+ *          <h1>i am h1 tag</h1>
+ *          <h2>i am h2 tag</h2>
+ *      </div>
+ *      <div="child2">
+ *          <h1>i am h1 tag</h1>
+ *          <h2>i am h2 tag</h2>
+ *      </div>
+ * </div>
+ */
+
+
+// const heading = React.createElement(
+//     "h1", 
+//      {id:"heading", abc:"xyz"}, 
+//      "hello world from react");
+
+const heading = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement("div",
+            { id: "child1" },
+            [
+                React.createElement("h1", {}, "I am h1 tag"),
+                React.createElement("h2", {}, "I am h2 tag")
+            ]
+        ),
+        React.createElement("div",
+            { id: "child2" },
+            [
+                React.createElement("h1", {}, "I am h1 tag"),
+                React.createElement("h2", {}, "I am h2 tag")
+            ]
+        )
+    ]
+)
+
+console.log(heading)
+const root1 = ReactDOM.createRoot(document.getElementById("root"))
+root1.render(heading)
